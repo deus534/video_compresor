453ffbbfedf1ee4991b2842db8fa827c92a4a7c5
brian <brianrs536@gmail.com>
2025-01-09 10:20:32 -0300

    agregando funcionalidades

--- ver_2/main.py
+++ ver_2/main.py
@@ -2,6 +2,7 @@ from PIL import Image
 import numpy as np
 import math as mt
 import os
+import cv2
 
 #ctes globales
 path_images = 'Images/'
@@ -27,13 +28,13 @@ def save_report( file_path, number_images ):
 def create_images( file_name, width=1360, height=768 ):
     chunk_size = width * height
     total_pixels = width * height
-    total_images = mt.ceil(len(data) / total_pixels)
-    path_save = save_report(file_name, total_images)
 
     with open(file_name, 'rb') as f:
         #while chunk:= f.read(chunk_size):
             #proceso de datos.
         data = f.read()
+    total_images = mt.ceil(len(data) / total_pixels)
+    path_save = save_report(file_name, total_images)
 
     for i in range(total_images):
         start = i * total_pixels
@@ -72,9 +73,42 @@ def create_file(file_report):
                     if total_written >= size_file:
                         break
 
+def create_video_from_images(image_folder, output_video, frame_rate=30):
+    # Obtener la lista de imágenes en la carpeta
+    images = [img for img in os.listdir(image_folder) if img.endswith((".png", ".jpg", ".jpeg"))]
+    images.sort()  # Asegurarse de que estén ordenadas numéricamente o alfabéticamente
+
+    # Leer la primera imagen para obtener el tamaño del video
+    first_image_path = os.path.join(image_folder, images[0])
+    frame = cv2.imread(first_image_path)
+    height, width, layers = frame.shape
+
+    # Configurar el VideoWriter
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")  # Codec para archivo MP4
+    video = cv2.VideoWriter(output_video, fourcc, frame_rate, (width, height))
+
+    # Loop a través de todas las imágenes y agregar cada una al video
+    for image in images:
+        image_path = os.path.join(image_folder, image)
+        frame = cv2.imread(image_path)
+
+        # Asegúrate de que todas las imágenes tienen la misma resolución
+        if frame.shape[:2] != (height, width):
+            frame = cv2.resize(frame, (width, height))
+
+        video.write(frame)  # Escribir el frame en el video
+
+    # Liberar el VideoWriter
+    video.release()
+    print(f"Video creado correctamente: {output_video}")
+
 if __name__ == '__main__':
-    file_path = input('enter the file name: ')
-    file_name = os.path.splitext(os.path.basename(file_path))[0]
-    create_images(path_files+file_path)
-    create_file(path_images+ file_name +'/info.txt')
+    #file_path = input('enter the file name: ')
+    #file_name = os.path.splitext(os.path.basename(file_path))[0]
+    #tarda en crear las imagenes....
+    #create_images(path_files+file_path)
+    #rapido para reconstruirlo
+    #create_file(path_images+ file_name +'/info.txt')
+
+    create_video_from_images('Images/video/images/', 'output_video.mkv', frame_rate=30)
     print('end')
